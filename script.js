a27bb5a6dfb27985f616210dcda3a70c0959187c
Zac Jones <zacjones93@gmail.com>
2017-08-15 09:25:33 -0700

    updated for plunker

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-xs.periodic(1000)                  // ---!---!---!---!---
-.fold(prev => prev + 1, 0)         // 0--1---2---3---4---
-.map(i => `Seconds elapsed: ${i}`)
-.subscribe({
+// Logic
+function main() {
+    return xs.periodic(1000)                       
+            .fold(prev => prev + 1, 0)        
+            .map(i => `Seconds elapsed: ${i}`)
+}
+
+function domDriver(text$) {
+   text$.subscribe({
     next: str => {
         const elem = document.querySelector('#app');
         elem.textContent = str;
-    }
-})
+     }
+    })  
+}
+
+function logDriver(msg$) {
+    msg$.subscribe({
+        next: msg => {
+            console.log(msg);
+        }
+    })
+}
+
+const sink = main();
+domDriver(sink);
+logDriver(sink);
