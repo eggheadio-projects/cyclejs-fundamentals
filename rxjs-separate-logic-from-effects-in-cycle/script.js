a06f4d5d088e2f0dd44c70ab7cabc37f501c1e31
Texas Toland <texastoland@users.noreply.github.com>
2018-09-26 19:05:11 -0500

    Remove default example

--- rxjs-separate-logic-from-effects-in-cycle/script.js
+++
@@ -1,9 +0,0 @@
-xs.periodic(1000)                  // ---!---!---!---!---
-.fold(prev => prev + 1, 0)         // 0--1---2---3---4---
-.map(i => `Seconds elapsed: ${i}`)
-.subscribe({
-    next: str => {
-        const elem = document.querySelector('#app');
-        elem.textContent = str;
-    }
-})
